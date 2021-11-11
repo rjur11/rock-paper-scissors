8b55da97ba47a197b162f177b7cdad4cdff05234
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-11-10 21:46:06 -0500

    Added hard mode button functionality

--- main.js
+++ main.js
@@ -3,17 +3,24 @@ var classicView = document.querySelector(".classic-game-view");
 var hardView = document.querySelector(".hard-game-view");
 var homeViewButtons = document.querySelector(".buttons");
 var classicButton = document.querySelector(".easy-game");
+var hardButton = document.querySelector(".hard-game");
 var bulbaButton = document.querySelector(".bulba");
 var charmButton = document.querySelector(".charm");
 var squirtButton = document.querySelector(".squirt");
 var normalPokesPage = document.querySelector(".normal-game-view");
 
 classicButton.addEventListener("click", classicMode);
+hardButton.addEventListener("click", hardMode);
 
 function classicMode() {
   addHidden(homeViewButtons);
   removeHidden(normalPokesPage);
   console.log("clicked");
+}
+
+function hardMode() {
+  addHidden(homeViewButtons);
+  removeHidden(hardView);
 }
 
 function addHidden(element) {
