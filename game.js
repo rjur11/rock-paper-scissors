f1d22c51e5f5b842e615cb2bc9f043cd8c822d22
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-11-11 15:54:30 -0500

    Build up game class

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor(player1, player2, gameBoard, gameType) {
+    this.player1 = new Player({name: name, token: token, wins: wins});
+    this.player2 = new Player({name: name, token: token, wins: wins});
+    this.gameBoard = [];
+    this.gameType = undefined;
+  }
+  checkForWin() {}
+  resetBoard() {}
+}
