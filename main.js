8135117a18d0fefc20f20dd417bf9cdc3f1be3d8
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-11-11 16:16:39 -0500

    Pull out more elements from HTML

--- main.js
+++ main.js
@@ -7,20 +7,31 @@ var hardButton = document.querySelector(".hard-game");
 var bulbaButton = document.querySelector(".bulba");
 var charmButton = document.querySelector(".charm");
 var squirtButton = document.querySelector(".squirt");
+var pikaButton = document.querySelector(".pikachu");
+var sandButton = document.querySelector(".sand");
 var normalPokesPage = document.querySelector(".normal-game-view");
+var subHeader = document.querySelector(".sub-header");
 
 classicButton.addEventListener("click", classicMode);
 hardButton.addEventListener("click", hardMode);
+bulbaButton.addEventListener("click", makeSelection);
+charmButton.addEventListener("click", makeSelection);
+squirtButton.addEventListener("click", makeSelection);
+pikaButton.addEventListener("click", makeSelection);
+sandButton.addEventListener("click", makeSelection);
 
 function classicMode() {
   addHidden(homeViewButtons);
   removeHidden(normalPokesPage);
+  subHeader.innerText = "Choose your pokemon!";
   console.log("clicked");
 }
 
 function hardMode() {
   addHidden(homeViewButtons);
   removeHidden(hardView);
+  subHeader.innerText = "Choose your pokemon!";
+  console.log("clicked");
 }
 
 function addHidden(element) {
@@ -30,3 +41,11 @@ function addHidden(element) {
 function removeHidden(element) {
   element.classList.remove("hidden");
 }
+
+function makeSelection(element) {
+  console.log(`You have selected ${element.target}`);
+}
+
+// function computerPick(array){
+//   return Math.floor(Math.random() * array.length)
+// }
